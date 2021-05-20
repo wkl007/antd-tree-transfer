ab0147d3f6861981a94d405b79b2774266c39dd2
wkl007 <499657357@qq.com>
2021-05-20 14:17:14 +0800

    feat: 项目初始化

---
+++ .stylelintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order'
+  ],
+  plugins: [
+    'stylelint-order'
+  ],
+  rules: {
+    'selector-pseudo-class-no-unknown': null,
+    'no-descending-specificity': null,
+    'at-rule-no-unknown': null,
+    'font-family-no-missing-generic-family-keyword': null,
+    'selector-type-no-unknown': null,
+    'function-calc-no-invalid': null,
+    'selector-pseudo-element-no-unknown': null
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style lang="less">
+#app {
+  margin-top: 60px;
+  color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
